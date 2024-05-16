ada35329bebc65b514567add99567a6d2c3350d9
Joseph Rodriguez <josephrodriri@gmail.com>
2024-05-15 23:46:40 -0500

    TareaDos

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1;
+let num2;
+let resultado;
+alert ("Suma de numeros");
+num1=Number(prompt("Ingrese numero uno"));
+num2=Number(prompt("Ingrese numero dos"));
+
+resultado = (num1+num2);
+
+alert (`el resultado de ${num1} y ${num2} es ${resultado}`)
