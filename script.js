db9325b8ba3ba78540488350ab2bb3b1eed5e56a
Joseph Rodriguez <josephrodriri@gmail.com>
2024-05-16 00:02:06 -0500

    actualizacion

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let num1;
-let num2;
-let resultado;
-alert ("Suma de numeros");
-num1=Number(prompt("Ingrese numero uno"));
-num2=Number(prompt("Ingrese numero dos"));
+let num1 = 5;
+let num2 = 7;
+let suma;
+let resta;
+let multiplicacion;
+let division;
 
-resultado = (num1+num2);
+suma = (num1+num2);
+resta = ( num1-num2);
+multiplicacion = (num1*num2);
+division = (num1/num2);
 
-alert (`el resultado de ${num1} y ${num2} es ${resultado}`)
+console.log("la suma es "+suma)
+console.log("la resta es "+resta)
+console.log("la multiplicacion es "+multiplicacion)
+console.log("la division es "+division)
+
+
